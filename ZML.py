063ccab813ceec4fb695f27043404c15810b5c84
Hema sagar Endluri <56343742+sagarendluri@users.noreply.github.com>
2024-06-26 13:48:20 -0400

    Update ZML.py

--- ZML.py
+++ ZML.py
@@ -17,14 +17,28 @@ pdfs = ['Doctor_prescription_files/Prescription_1.pdf']#, 'Doctor_prescription_f
 #             paths = pdf_paths + file
 #             pdf.append(paths)
 st.title("Chat with ZML file Patient data file.")
-# secret_value = secrets.get_secret("DEPLOYMENT_NAME")
-# print(f"deployment_key:{secret_value}")
+embed_model = AzureAIEmbeddings(
+    endpoint_url="https://marketplace.openai.azure.com/",
+    azure_key="d6d9522a01c74836907af2f3fd72ff85",
+    api_version="2024-02-01",
+    deployment_name_embeddings="text-embed-marketplace"
+)
+BASE_URL = "https://gpt-res.openai.azure.com/"
+DEPLOYMENT_NAME= "gpt-4-32k" 
+API_KEY = "a20bc67dbd7c47ed8c978bbcfdacf930"
 
-
-deployment = os.environ.get('deployment_name')
-print(deployment)
-# auth_header_encoded = base64.b64decode(f"{DEPLOYMENT_NAME}".encode("ascii"))
-# auth_header = f"Basic{auth_header_encoded.decode('ascii')}"
-# deployment_name = os.getenv("DEPLOYMENT_NAME")
-
+data = source.fit(path=pdfs, dtype="pdf",chunk_size=512,chunk_overlap=20)
+# text_embedding = embed_model.embed_text(str(data))
+retriever = retrieve.auto_retriever(data,embed_model=embed_model,type="normal",top_k=4)
+llm = AzureOpenAIModel(model="gpt4",azure_key = API_KEY,deployment_name="gpt-4-32k" ,endpoint_url=BASE_URL,model_kwargs={"max_tokens":512,"temperature":0.1})
+# option = st.selectbox( 'Please Select the Patient name?', ('Bobby Jackson', 'Leslie Terry','Danny Smith'))
+question = st.text_input("Enter your question")
+# question = "what is the Bobby Jackson condition?"
+submit=st.button("Get the data")
+if submit:
+    print(question)
+    pipeline = generator.Generate(question=question, retriever=retriever, llm=llm)
+    response = pipeline.call()
+    st.write(response)
+    
 
