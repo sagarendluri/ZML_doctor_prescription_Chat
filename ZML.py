923c1b8027d1f9f6bc4bc11dbb9ddd384d43a871
Hema sagar Endluri <56343742+sagarendluri@users.noreply.github.com>
2024-06-26 15:50:01 -0400

    Update ZML.py

--- ZML.py
+++ ZML.py
@@ -18,41 +18,41 @@ pdfs = ['Doctor_prescription_files/Prescription_1.pdf', 'Doctor_prescription_fil
 #             pdf.append(paths)
 st.title("Chat with ZML file Patient data file.")
 
-print(st.secrets[endpoint_url])
-headers = {
+# print(st.secrets[endpoint_url])
+# headers = {
 
-    endpoint_url = st.secrets[endpoint_url],
-    azure_key = st.secrets[azure_key],
-    api_version= st.secrets[api_version],
-    deployment_name=st.secrets[deployment_name]
+#     endpoint_url = st.secrets[endpoint_url],
+#     azure_key = st.secrets[azure_key],
+#     api_version= st.secrets[api_version],
+#     deployment_name=st.secrets[deployment_name]
     
-}
+# }
 
-embed_model = AzureAIEmbeddings(
-    endpoint_url = st.secrets[endpoint_url],
-    azure_key = st.secrets[azure_key],
-    api_version= st.secrets[api_version],
-    deployment_name=st.secrets[deployment_name]
-)
-BASE_URL = st.secrets[BASE_URL]
-DEPLOYMENT_NAME= st.secrets[DEPLOYMENT_NAME] 
-API_KEY = st.secrets[API_KEY]
+# embed_model = AzureAIEmbeddings(
+#     endpoint_url = st.secrets[endpoint_url],
+#     azure_key = st.secrets[azure_key],
+#     api_version= st.secrets[api_version],
+#     deployment_name=st.secrets[deployment_name]
+# )
+# BASE_URL = st.secrets[BASE_URL]
+# DEPLOYMENT_NAME= st.secrets[DEPLOYMENT_NAME] 
+# API_KEY = st.secrets[API_KEY]
 
-data = source.fit(path=pdfs, dtype="pdf",chunk_size=512,chunk_overlap=20)
-# text_embedding = embed_model.embed_text(str(data))
-retriever = retrieve.auto_retriever(data,embed_model=embed_model,type="normal",top_k=4)
-llm = AzureOpenAIModel(model="gpt4",azure_key = API_KEY,deployment_name="gpt-4-32k" ,endpoint_url=BASE_URL,model_kwargs={"max_tokens":512,"temperature":0.1})
-# option = st.selectbox( 'Please Select the Patient name?', ('Bobby Jackson', 'Leslie Terry','Danny Smith'))
-question = st.text_input("Enter your question")
-# question = "what is the Bobby Jackson condition?"
+# data = source.fit(path=pdfs, dtype="pdf",chunk_size=512,chunk_overlap=20)
+# # text_embedding = embed_model.embed_text(str(data))
+# retriever = retrieve.auto_retriever(data,embed_model=embed_model,type="normal",top_k=4)
+# llm = AzureOpenAIModel(model="gpt4",azure_key = API_KEY,deployment_name="gpt-4-32k" ,endpoint_url=BASE_URL,model_kwargs={"max_tokens":512,"temperature":0.1})
+# # option = st.selectbox( 'Please Select the Patient name?', ('Bobby Jackson', 'Leslie Terry','Danny Smith'))
+# question = st.text_input("Enter your question")
+# # question = "what is the Bobby Jackson condition?"
 
-system_prompt = "You are acting like a chat...."
+# system_prompt = "You are acting like a chat...."
 
-submit=st.button("Get the data")
-if submit:
-    print(question)
-    pipeline = generator.Generate(question=question, retriever=retriever,system_prompt=system_prompt, llm=llm)
-    response = pipeline.call()
-    st.write(response)
+# submit=st.button("Get the data")
+# if submit:
+#     print(question)
+#     pipeline = generator.Generate(question=question, retriever=retriever,system_prompt=system_prompt, llm=llm)
+#     response = pipeline.call()
+#     st.write(response)
     
 
