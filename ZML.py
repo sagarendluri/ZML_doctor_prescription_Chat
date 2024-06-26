6661560de41a1520805db89ef6ae7445a08abcf1
Hema sagar Endluri <56343742+sagarendluri@users.noreply.github.com>
2024-06-25 23:56:56 -0400

    Update ZML.py

--- ZML.py
+++ ZML.py
@@ -25,7 +25,7 @@ BASE_URL = "https://gpt-res.openai.azure.com/"
 DEPLOYMENT_NAME= "gpt-4-32k" 
 API_KEY = "a20bc67dbd7c47ed8c978bbcfdacf930"
 # option = st.selectbox( 'Please Select the Patient name?', ('Bobby Jackson'), 'Leslie Terry','Danny Smith'))
-st.title("Chat with ZML file Patient data file of 'Bobby Jackson'.")
+st.title("Chat with Prescription Patient data file of 'Bobby Jackson'.")
 data = source.fit(path=pdfs, dtype="pdf",chunk_size=512,chunk_overlap=20)
 # text_embedding = embed_model.embed_text(str(data))
 retriever = retrieve.auto_retriever(data,embed_model=embed_model,type="normal",top_k=4)
