e03ff9abd53a3f668383def25dcec203eeb74d57
Hema sagar Endluri <56343742+sagarendluri@users.noreply.github.com>
2024-06-26 11:42:35 -0400

    Update ZML.py

--- ZML.py
+++ ZML.py
@@ -21,7 +21,7 @@ pdfs = ['Doctor_prescription_files/Prescription_1.pdf']#, 'Doctor_prescription_f
 endpoint_url = os.getenv('ENDPOINT_URL')
 azure_key = os.getenv('AZURE_KEY')
 api_version = os.getenv('API_VERSION')
-deployment_name_embeddings = os.getenv('DEPLOYMENT_NAME_EMBEDDINGS')
+deployment_name_embeddings = "" #os.getenv('DEPLOYMENT_NAME_EMBEDDINGS')
 
 print(endpoint_url, azure_key, api_version, deployment_name_embeddings)
 # embed_model = AzureAIEmbeddings(endpoint_url=endpoint_url, azure_key=azure_key, api_version=api_version, deployment_name=deployment_name_embeddings)
@@ -35,19 +35,19 @@ system_prompt = "You should act like an Chatbot...."
 # option = st.selectbox( 'Please Select the Patient name?', ('Bobby Jackson'), 'Leslie Terry','Danny Smith'))
 st.title("Chat with Prescription Patient data file of 'Bobby Jackson'.")
 data = source.fit(path=pdfs, dtype="pdf",chunk_size=512,chunk_overlap=20)
-# text_embedding = embed_model.embed_text(str(data))
-# retriever = retrieve.auto_retriever(data,embed_model=embed_model,type="normal",top_k=4)
-# llm = AzureOpenAIModel(model="gpt4",azure_key = API_KEY,deployment_name="gpt-4-32k" ,endpoint_url=BASE_URL,model_kwargs={"max_tokens":512,"temperature":0.1})
+text_embedding = embed_model.embed_text(str(data))
+retriever = retrieve.auto_retriever(data,embed_model=embed_model,type="normal",top_k=4)
+llm = AzureOpenAIModel(model="gpt4",azure_key = API_KEY,deployment_name="gpt-4-32k" ,endpoint_url=BASE_URL,model_kwargs={"max_tokens":512,"temperature":0.1})
 
-# question = st.text_input("Enter your question")
-# # question = "what is the Bobby Jackson condition?"
-# submit=st.button("Get the data")
+question = st.text_input("Enter your question")
+# question = "what is the Bobby Jackson condition?"
+submit=st.button("Get the data")
 
-# if submit:
-#     print(question)
-#     pipeline = generator.Generate(question=question, retriever=retriever,system_prompt=system_prompt, llm=llm)
-#     response = pipeline.call()
-#     st.write(response)
+if submit:
+    print(question)
+    pipeline = generator.Generate(question=question, retriever=retriever,system_prompt=system_prompt, llm=llm)
+    response = pipeline.call()
+    st.write(response)
     
 
 
