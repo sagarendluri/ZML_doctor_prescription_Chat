0b0146210d580ba8db6a4529a52da8a2841b8432
Hema sagar Endluri <56343742+sagarendluri@users.noreply.github.com>
2024-06-25 23:15:41 -0400

    Update ZML.py

--- ZML.py
+++ ZML.py
@@ -7,12 +7,14 @@ from beyondllm import source
 import os
 pdf = [ ]
 pdf_paths = "/Doctor_prescription_files/"
-dir = os.walk(pdf_paths)
-for root, dir,files  in dir:
-    for file in files:
-        if file.endswith('.pdf'):
-            paths = pdf_paths + file
-            pdf.append(paths)
+
+pdfs = ['/Doctor_prescription_files/Prescription_1.pdf', '/Doctor_prescription_files/Prescription_3.pdf', '/Doctor_prescription_files/Prescription_2.pdf']
+# dir = os.walk(pdf_paths)
+# for root, dir,files  in dir:
+#     for file in files:
+#         if file.endswith('.pdf'):
+#             paths = pdf_paths + file
+#             pdf.append(paths)
 embed_model = AzureAIEmbeddings(
     endpoint_url="https://marketplace.openai.azure.com/",
     azure_key="d6d9522a01c74836907af2f3fd72ff85",
@@ -25,7 +27,7 @@ API_KEY = "a20bc67dbd7c47ed8c978bbcfdacf930"
 
 # question = "what is the Bobby Jackson condition?"
 # def llmmodels(question):
-data = source.fit(path=pdf, dtype="pdf",chunk_size=512,chunk_overlap=20)
+data = source.fit(path=pdfs, dtype="pdf",chunk_size=512,chunk_overlap=20)
 # text_embedding = embed_model.embed_text(str(data))
 retriever = retrieve.auto_retriever(data,embed_model=embed_model,type="normal",top_k=4)
 llm = AzureOpenAIModel(model="gpt4",azure_key = API_KEY,deployment_name="gpt-4-32k" ,endpoint_url=BASE_URL,model_kwargs={"max_tokens":512,"temperature":0.1})
